c03f6ed7b415f64c8f855e160c355bccb844922f
Dan Torrey <dan@graylog.com>
2019-01-04 13:04:30 -0600

    Build JSON with library instead of building with strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,36 +2,47 @@ import com.ning.http.client.AsyncCompletionHandler;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.ListenableFuture;
 import com.ning.http.client.Response;
+import org.json.JSONObject;
 
 import java.util.Random;
 
 public class Main {
 
-    public static final String GRYLOG_INPUT_URI = "http://127.0.0.1:16000/gelf";
-    static AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    private static final String GRYLOG_INPUT_URI = "http://127.0.0.1:16000/gelf";
+    private static AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
     public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i < 20000; i++) {
+        for (int i = 0; i < 10; i++) {
 
             // Random delay to slow sending down a bit.
             Thread.sleep(new Random().nextInt(400));
-
-            String content = "{\n" +
-                             "  \"version\": \"1.1\",\n" +
-                             "  \"host\": \"example.org\",\n" +
-                             "  \"short_message\": \"A short message that helps you identify what is going on\",\n" +
-                             "  \"full_message\": \"Backtrace here\\n\\nmore stuff\",\n" +
-                             "  \"level\": 1,\n" +
-                             "  \"_user_id\": 9001,\n" +
-                             "  \"_some_info\": \"foo\",\n" +
-                             "  \"_source_address\": \"10.0.0.0\",\n" +
-                             "  \"_some_env_var\": \"bar\"\n" +
-                             "}";
-
+            String content = buildContent();
             sendRequest(content);
         }
     }
+
+    private static String buildContent() {
+
+        JSONObject json = new JSONObject();
+        json.put("version", "1.1");
+        json.put("host", "example.org");
+        json.put("short_message", "A sample message to test TOR and Reverse DNS lookups");
+        json.put("full_message", "More stuff here");
+        json.put("level", 1);
+        json.put("_user_id", 9001);
+        json.put("_some_info", "foo");
+        json.put("_src_addr", "10.0.0.1");
+        json.put("_mac_addr", "ACBCC1212");
+        json.put("_dest_addr", "" + randomIp());
+        json.put("_user_id", randomInt());
+        json.put("_some_env_var", "bar");
+
+        System.out.println(String.format("Request JSON [%s]", json));
+
+        return json.toString();
+    }
+
 
     private static void sendRequest(String content) throws InterruptedException, java.util.concurrent.ExecutionException {
 
@@ -40,24 +51,28 @@ public class Main {
                                                            .execute(new AsyncCompletionHandler<Response>() {
 
                                                                @Override
-                                                               public Response onCompleted(Response response) throws Exception {
+                                                               public Response onCompleted(Response response) {
                                                                    // Do nothing with the response
                                                                    return response;
                                                                }
 
                                                                @Override
                                                                public void onThrowable(Throwable t) {
-                                                                   // Something bad happened.
-                                                                   System.out.println(t);
+                                                                   System.out.println("Something bad happened");
                                                                }
                                                            });
 
         Response response = future.get();
-        System.out.println(response.getStatusCode());
+        System.out.println(String.format("Response code [%d]", response.getStatusCode()));
     }
 
     private static String randomIp() {
         Random r = new Random();
         return r.nextInt(191) + "." + r.nextInt(254) + "." + r.nextInt(254) + "." + r.nextInt(254);
+    }
+
+    private static int randomInt() {
+        Random r = new Random();
+        return r.nextInt(1000);
     }
 }
