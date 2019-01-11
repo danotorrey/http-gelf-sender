2dee4385b2fe407de248a529e990ec929d93146f
Dan Torrey <dan@graylog.com>
2019-01-11 10:35:25 -0600

    Use Spring RestTemplate to send post request
    This simplifies the code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,60 @@
-import com.ning.http.client.AsyncCompletionHandler;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.ListenableFuture;
-import com.ning.http.client.Response;
+
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
 
 public class Main {
 
-    private static final String GRYLOG_INPUT_URI = "http://127.0.0.1:16000/gelf";
-    private static AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    private static final int RANDOM_DELAY_BOUND = 400;
+    private static final String GRAYLOG_INPUT_URI = "http://127.0.0.1:16001/gelf";
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100000; i++) {
 
             // Random delay to slow sending down a bit.
-            Thread.sleep(new Random().nextInt(400));
-            String content = buildContent();
-            sendRequest(content);
+            Thread.sleep(new Random().nextInt(RANDOM_DELAY_BOUND));
+
+            // Build the JSON string
+            String postBody = buildJson();
+
+            // Set the content type, so Graylog knows we're going to send JSON.
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            // Prepare and send the request.
+            HttpEntity<String> entity = new HttpEntity<String>(postBody, headers);
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Send it.
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(GRAYLOG_INPUT_URI, entity, String.class);
+
+            // Handle response.
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                LOG.info("Success: Response code [{}]", responseEntity.getStatusCodeValue());
+            } else {
+                LOG.info("Error: response code [{}], reason [{}].",
+                         responseEntity.getStatusCodeValue(),
+                         responseEntity.getStatusCode().getReasonPhrase());
+            }
         }
     }
 
-    private static String buildContent() {
+    private static String buildJson() {
 
         JSONObject json = new JSONObject();
         json.put("version", "1.1");
         json.put("host", "example.org");
-        json.put("short_message", "A sample message to test TOR and Reverse DNS lookups");
+        json.put("short_message", "AA sample message to test TOR and Reverse DNS lookups");
         json.put("full_message", "More stuff here");
         json.put("level", 1);
         json.put("_user_id", 9001);
@@ -40,32 +63,10 @@ public class Main {
         json.put("_mac_addr", "ACBCC1212");
         json.put("_dest_addr", "" + randomIp());
         json.put("_user_id", randomInt());
-        json.put("_some_env_var", "bar");
-        LOG.info("Request JSON {}", json);
+
+        // LOG.info("Request JSON {}", json);
 
         return json.toString();
-    }
-
-    private static void sendRequest(String content) throws InterruptedException, java.util.concurrent.ExecutionException {
-
-        ListenableFuture<Response> future = asyncHttpClient.preparePost(GRYLOG_INPUT_URI)
-                                                           .setBody(content)
-                                                           .execute(new AsyncCompletionHandler<Response>() {
-
-                                                               @Override
-                                                               public Response onCompleted(Response response) {
-                                                                   // Do nothing with the response
-                                                                   return response;
-                                                               }
-
-                                                               @Override
-                                                               public void onThrowable(Throwable t) {
-                                                                   System.out.println("Something bad happened");
-                                                               }
-                                                           });
-
-        Response response = future.get();
-        LOG.info("Response code {}", response.getStatusCode());
     }
 
     private static String randomIp() {
